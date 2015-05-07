0421b13a423c1faccb326c6b6e1fb8d8a086bbc8
Adam Crawford <geeksbsmrt@gmail.com>
2015-05-07 18:24:38 -0400

    Rebrand Project

---
+++ android/src/com/adamcrawford/poncer/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.adamcrawford.poncer.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.adamcrawford.poncer.Poncer;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Poncer(), config);
+	}
+}
